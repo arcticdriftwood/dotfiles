7f10e535ac2ce39a42bbf8716a098989a531fa44
driftwood <arcticdriftwood@gmail.com>
2018-05-14 10:27:49 -0500

    push before changes

--- home--scripts--spawn-floating-urxvtc.sh
+++ home--scripts--spawn-floating-urxvtc.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# A cobbled-together floating terminal.
+# from an r/bspwm thread, %s/xdo/xdotool/, hide -> windowunmap...
 
-if [ -f ~/.floating-urxvtc-open ]; 
-then
-	kill $(cat ~/.floating-urxvtc-open)
-	rm ~/.floating-urxvtc-open
+id=$(xdotool search --name dropdown);
+if [ -z "$id" ]; then
+	urxvtc -name dropdown;
 else
-	urxvtc
-	bspc node -f newer
-	bspc node -t floating
-	bspc node -z bottom 0 -161
-	bspc node -z top    0  161
-	bspc node -g sticky
-	echo $(pgrep -nP $(pgrep urxvtd)) > ~/.floating-urxvtc-open
+	cmd='windowunmap';
+	state=$(xprop -id $id | awk '/window state:/ {print $3}');
+	if [[ "$state" == "Withdrawn" ]]; then
+		cmd='windowmap';
+	fi
+	xdotool $cmd $id
 fi
--- home--scripts--wifi-dmenu.sh
+++ home--scripts--wifi-dmenu.sh
@@ -1,14 +1,7 @@
 WIFIFILE=$(ls ~/wifi | sed s/\ /\n/ | dmenu -p "connect-to:" -fn "Tamsyn" -nb "#2E3440" -nf "#D8DEE9" -sb "#2E3440" -sf "#A3BE8C")
 
 if [ ! $WIFIFILE = "" ]; 
-then urxvtc -e sh -c \
-	"bspc node --focus  newer;            \
-	 bspc node --state  floating;         \
-	 bspc node --resize top       0  161; \
-	 bspc node --resize bottom    0 -161; \
-	 bspc node --resize right  -245    0; \
-	 bspc node --resize left    245    0; \
-	 bspc node --move          -601 -389; \
-	 sudo killall wpa_supplicant; \
+then urxvtc -name "wifi-dmenu" -e sh -c \
+	 "sudo killall wpa_supplicant; \
 	 sudo wpa_supplicant -B -iwlp2s0 -c ~/wifi/$WIFIFILE";
 fi
